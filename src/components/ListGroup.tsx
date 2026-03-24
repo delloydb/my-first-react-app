751fe8e3799c02483dfb82a0eeea3d5e13581bf0
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-03-24 15:32:42 +0300

    dynamic list updated

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,20 @@
 import { Fragment } from "react/jsx-runtime";
 function ListGroup() {
+  const items = [
+    "An item",
+    "A second item",
+    "A third item",
+    "A fourth item",
+    "And a fifth one",
+  ];
+
   return (
     <Fragment>
       <h1>List Group</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
+        {items.map((item) => (
+          <li className="list-group-item">{item}</li>
+        ))}
         <li className="list-group-item">A second item</li>
         <li className="list-group-item">A third item</li>
         <li className="list-group-item">A fourth item</li>
