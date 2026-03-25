20feef19786d6dea3d267fab2c4bae7d3e561ad6
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-03-25 22:28:52 +0300

    initial commit

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -15,10 +15,6 @@ function ListGroup() {
         {items.map((item) => (
           <li className="list-group-item">{item}</li>
         ))}
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
       </ul>
     </Fragment>
   );
